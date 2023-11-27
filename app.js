a3d344bad3b5a32ce6dc951415225684ab8f7cc2
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-27 17:59:09 +0700

    Fetch random dad joke from API

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
 btn.addEventListener("click", () => {
-  console.log("hello");
+  fetchDadJoke();
 });
+
+const fetchDadJoke = async () => {
+  const resp = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "learning app",
+    },
+  });
+  const data = await resp.json();
+  result.textContent = data.joke;
+};
+fetchDadJoke();
